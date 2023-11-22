21d33e3ece09d977e06b4f7b6525d82560cbabb8
Fausta Rizky Abriansah <semarangkiki40@gmail.com>
2023-11-22 17:17:29 +0700

    UTS Android G.111.22.0007

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -31,21 +31,24 @@ public class MainActivity extends AppCompatActivity {
         btnSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String isian_nama_depan = edNamaDepan.getText().toString();
-                String isian_nama_belakang = edNamaBelakang.getText().toString();
 
-                if(isian_nama_depan.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
-                    daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
-                    edNamaDepan.setText("");
-                    edNamaBelakang.setText("");
-                    intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
-                    startActivity(intent_list);
+                    String isian_nama_depan = edNamaDepan.getText().toString();
+                    String isian_nama_belakang = edNamaBelakang.getText().toString();
+
+                    if (isian_nama_depan.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
+                        daftar_nama.clear();
+                        daftar_nama.add(nama_lengkap);
+                        edNamaDepan.setText("");
+                        edNamaBelakang.setText("");
+                        intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
+                        startActivity(intent_list);
+                    }
+
                 }
-            }
+
         });
     }
 }
